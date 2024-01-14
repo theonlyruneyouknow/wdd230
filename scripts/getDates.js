228535ebd11d109eed6495961d09b9726646374f
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-01-13 22:19:52 -0800

    done for today

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,5 @@
 let text = "Last modified: " + document.lastModified;
 document.getElementById("lastModified2").innerHTML = text;
 
-let yearnow = document.write(new Date().getFullYear() + "@ Rune Larsen, Oregon, USA");
+let yearnow = document.write(new Date().getFullYear() + "\u00A9 Rune Larsen, Oregon, USA");
 document.getElementById("thisyear").innerHTML = yearnow;
