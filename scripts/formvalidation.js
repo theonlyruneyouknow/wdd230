4ed5327546136cfe1515e2831cde1e401ed08f05
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-01-31 21:53:15 -0800

    almo

--- scripts/formvalidation.js
+++ scripts/formvalidation.js
@@ -1,14 +1,25 @@
-const passwd = document.querySelector("#password");
-const passwd2 = document.querySelector("#password2");
-const errormsg = document.querySelector("msgWarning");
+// const passwd = document.querySelector("#password");
+// const passwd2 = document.querySelector("#password2");
+// const errormsg = document.querySelector("msgWarning");
 
-/* compare passwords */
-function checkPassWd() {
-    if (passwd.value !== passwd2.value) {
-        errormsg.textContent = "Password mis-match!!";
-        /* errormsg.computedStyleMap */
-    }
-    else {
+// /* compare passwords */
+// function checkPassWd() {
+//     if (passwd.value !== passwd2.value) {
+//         errormsg.textContent = "Password mis-match!!";
+//         /* errormsg.computedStyleMap */
+//     }
+//     else {
 
-    }
+//     }
+// }
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
 }
