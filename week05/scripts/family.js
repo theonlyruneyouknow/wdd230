af2be9bba8c28583c334b7973c7564b27ae53f70
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-09 21:04:44 -0800

    e3

--- week05/scripts/family.js
+++ week05/scripts/family.js
@@ -1,13 +1,15 @@
 const currentTemp = document.querySelector('#current-temp');
+const currentTemp2 = document.querySelector('#current-temp2');
+const currentTemp3 = document.querySelector('#current-temp3');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#figcaption');
 
 // const url = 'http://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=9e7bbf1ed19a6be3b7439a78e5e9e589&units=imperial';
-const url = '//theonlyruneyouknow.github.io/wdd230/week05/script/family.json';
+const url = 'http://127.0.0.1:5500/week05/scripts/family.json';
 
 async function apiFetch() {
     try {
-        const response = await fetch(url, { mode: 'no-cors' });
+        const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
             console.log(data); // testing only
@@ -21,8 +23,9 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-
+    currentTemp.innerHTML = `${data.member[0].householdMembers.length}&deg;F`;
+    currentTemp2.innerHTML = `${data.member[0].familyName}&deg;F`;
+    currentTemp3.innerHTML = `${data.member[0].householdMembers[0].name}&deg;F`;
     // description.innerHTML = `${data.weather[0].description}`;
 
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
--- week05/scripts/prophets2.js
+++ week05/scripts/prophets2.js
@@ -1,5 +1,6 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const url2 = 'https://theonlyruneyouknow.github.io/wdd230/data/links.json';
+const url3 = 'http://127.0.0.1:5500/week05/scripts/family.json';
 const cards = document.querySelector('#cards');
 
 // const newsection = document.createElement("section"); let and not const
@@ -13,28 +14,34 @@ const cards = document.querySelector('#cards');
 
 
 const displayLinks = (weeks) => {
-    weeks.forEach((week) => {
+    weeks.forEach((householdMembers) => {
         // Create elements to add to the div.cards element
 
         let card = document.createElement('section');
-        let links = document.createElement('h2'); // fill in the blank
+
+        let familyName = document.createElement('h2'); // fill in the blank
+        let links = document.createElement('p'); // fill in the blank
         let birthdate = document.createElement('p'); // fill in the blank
         let birthplace = document.createElement('p'); // fill in the blank
+        let first = document.createElement('p'); // fill in the blank
+
+
         let portrait = document.createElement('img');
 
         // Build the h2 content out to show the prophet's full name
-        first.textContent = `${weeks.week}`; // fill in the blank
-        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`; // fill in the blank
-        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`; // fill in the blank
-        // Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        first.textContent = `${weeks[0].links[0].title}`; // fill in the blank
+        // birthdate.textContent = `Date of Birth: ${prophet.birthdate}`; // fill in the blank
+        // birthplace.textContent = `Place of Birth: ${prophet.birthplace}`; // fill in the blank
+        // // Build the image portrait by setting all the relevant attributes
+        // portrait.setAttribute('src', prophet.imageurl);
+        // portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+        // portrait.setAttribute('loading', 'lazy');
+        // portrait.setAttribute('width', '340');
+        // portrait.setAttribute('height', '440');
 
         // Append the section(card) with the created elements
         card.appendChild(first); //fill in the blank
+        card.appendChild(familyName); //fill in the blank
         card.appendChild(birthdate); //fill in the blank
         card.appendChild(birthplace); //fill in the blank
         card.appendChild(portrait);
