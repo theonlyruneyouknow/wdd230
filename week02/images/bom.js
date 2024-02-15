d880da2ee6d5818a33a1b560dd9601ff575651f4
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-14 20:48:18 -0800

    b2

---
+++ week02/images/bom.js
@@ -0,0 +1,31 @@
+const input = document.queryselector('#favchap');
+const input = document.queryselector('#button');
+const input = document.queryselector('#list');
+
+button.addEventListener('click',
+    function () {
+
+        const deleteButton = 
+const li = document.createElement('li');
+        document.createElement('button');
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        li.textContent = input.value;
+        document.getElementById(favchap).innerHTML = sometext;
+	)
+}
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+
+input.focus();
+
+input.value = '';
+
+
+
+
+
+
