53e380a480e8f17c99a0c47ae230353e13d712a2
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-17 15:50:02 -0800

    events

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -17,22 +17,30 @@ const displaymembers = (members) => {
         let card = document.createElement('section');
         let Event = document.createElement('h2'); // fill in the blank
         let Webpage = document.createElement('p'); // fill in the blank
-        let Adress = document.createElement('p'); // fill in the blank
-        let Blurb = document.createElement('p'); // fill in the blank
+        let Location = document.createElement('p'); // fill in the blank
+        let Blurb = document.createElement('span'); // fill in the blank
+        let Theme = document.createElement('h3'); // fill in the blank
+        let Season = document.createElement('p'); // fill in the blank
+        let Date = document.createElement('p'); // fill in the blank
+        // let  = document.createElement('p'); // fill in the blank
+        // let  = document.createElement('p'); // fill in the blank
         let logo = document.createElement('img');
         let url = document.createElement('a');
         // let url = document.createElement('a');
 
         // Build the h2 content out to show the Member's full name
         Event.textContent = `${Member.Event} `; // fill in the blank
+        Theme.textContent = `${Member.Theme} `; // fill in the blank
+        Season.textContent = `${Member.Season} `; // fill in the blank
+        Date.textContent = `When: ${Member.Date} `; // fill in the blank
         Webpage.textContent = `Webpage: ${Member.Webpage}`; // fill in the blank
-        Adress.textContent = `Adress: ${Member.Adress}`; // fill in the blank
+        Location.textContent = `Location: ${Member.Location}`; // fill in the blank
         Blurb.textContent = `${Member.Blurb}`; // fill in the blank
         // Build the image logo by setting all the relevant attributes
         logo.setAttribute('src', Member.logo);
-        logo.setAttribute('alt', `logo of ${Member.Event} `); // fill in the blank
-        logo.setAttribute('alt', `logo of ${Member.Event} `); // fill in the blank
-        url.setAttribute('href', `https://${Member.Webpage}`);
+        logo.setAttribute('alt', `logo of ${Member.logo} `); // fill in the blank
+        logo.setAttribute('alt', `logo of ${Member.logo} `); // fill in the blank
+        url.setAttribute('href', `${Member.Webpage}`);
         logo.setAttribute('loading', 'lazy');
         logo.setAttribute('width', '340');
         // logo.setAttribute('height', '440');
@@ -40,9 +48,12 @@ const displaymembers = (members) => {
         // Append the section(card) with the created elements
         card.appendChild(logo);
         card.appendChild(Event);
-        card.appendChild(url); //fill in the blank
+        card.appendChild(Theme); //fill in the blank
+        // card.appendChild(Season); //fill in the blank
+        card.appendChild(Date); //fill in the blank
         // card.appendChild(Webpage); //fill in the blank
-        card.appendChild(Adress); //fill in the blank
+        card.appendChild(Location); //fill in the blank
+        card.appendChild(url); //fill in the blank
         card.appendChild(Blurb); //fill in the blank
 
         cards.appendChild(card);
