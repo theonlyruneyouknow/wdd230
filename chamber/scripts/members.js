beda59e1cd2c0532b3b601f2fc5b5aff4359bfb7
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-17 07:14:34 -0800

    prop to memb

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -7,8 +7,8 @@ const cards = document.querySelector('#cards');
 // create an img element and store it in a variable named "logo",
 // create an h2 element and store it in a variable named "fullName",
 
-const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
+const displaymembers = (members) => {
+    members.forEach((Member) => {
         // Create elements to add to the div.cards element
 
         let card = document.createElement('section');
@@ -20,20 +20,20 @@ const displayProphets = (prophets) => {
         let url = document.createElement('a');
         // let url = document.createElement('a');
 
-        // Build the h2 content out to show the prophet's full name
-        CompanyName.textContent = `${prophet.CompanyName} `; // fill in the blank
-        Webpage.textContent = `Webpage: ${prophet.Webpage}`; // fill in the blank
-        Adress.textContent = `Adress: ${prophet.Adress}`; // fill in the blank
-        Phone.textContent = `Phone: ${prophet.Phone}`; // fill in the blank
+        // Build the h2 content out to show the Member's full name
+        CompanyName.textContent = `${Member.CompanyName} `; // fill in the blank
+        Webpage.textContent = `Webpage: ${Member.Webpage}`; // fill in the blank
+        Adress.textContent = `Adress: ${Member.Adress}`; // fill in the blank
+        Phone.textContent = `Phone: ${Member.Phone}`; // fill in the blank
         // Build the image logo by setting all the relevant attributes
-        logo.setAttribute('src', prophet.logo);
-        logo.setAttribute('alt', `logo of ${prophet.CompanyName} `); // fill in the blank
-        logo.setAttribute('alt', `logo of ${prophet.CompanyName} `); // fill in the blank
-        url.setAttribute('href', `https://${prophet.Webpage}`);
+        logo.setAttribute('src', Member.logo);
+        logo.setAttribute('alt', `logo of ${Member.CompanyName} `); // fill in the blank
+        logo.setAttribute('alt', `logo of ${Member.CompanyName} `); // fill in the blank
+        url.setAttribute('href', `https://${Member.Webpage}`);
         logo.setAttribute('loading', 'lazy');
         logo.setAttribute('width', '340');
         // logo.setAttribute('height', '440');
-        url.innerHTML = prophet.Webpage;
+        url.innerHTML = Member.Webpage;
         // Append the section(card) with the created elements
         card.appendChild(logo);
         card.appendChild(CompanyName);
@@ -46,27 +46,27 @@ const displayProphets = (prophets) => {
     }); // end of arrow function and forEach loop
 }
 
-// const displayProphets = (prophets) => {
+// const displaymembers = (members) => {
 //     // card build code goes here
-//     prophets.forEach((prophet) => {
+//     members.forEach((Member) => {
 
 //     });
 // }
-// async function getProphetData() {
+// async function getMemberData() {
 //     console.log('calling');
 //     const result = await resolveAfter2Seconds();
 //     console.log(result);
 //     // Expected output: "resolved"
 //   }
-//   getProphetData();
+//   getMemberData();
 
-async function getProphetData() {
+async function getMemberData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets); // temporary testing of data retreival
-    displayProphets(data.prophets);
+    console.table(data.members); // temporary testing of data retreival
+    displaymembers(data.members);
 }
 
 
 
-getProphetData();
+getMemberData();
