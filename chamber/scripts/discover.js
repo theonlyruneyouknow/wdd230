143b95ad3c35ef4edce122dd221c53e7658d2d55
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-17 13:59:12 -0800

    discover swap

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,75 @@
+const url = 'data/discover.json';
+// const data = url.json()
+// console.log(data)
+// console.log(url);
+const cards = document.querySelector('#cards');
+
+// const newsection = document.createElement("section"); let and not const
+// const fullName = document.createElement("h2");let and not const
+// const logo = document.createElement("img");let and not const
+// create an img element and store it in a variable named "logo",
+// create an h2 element and store it in a variable named "fullName",
+
+const displaymembers = (members) => {
+    members.forEach((Member) => {
+        // Create elements to add to the div.cards element
+
+        let card = document.createElement('section');
+        let Event = document.createElement('h2'); // fill in the blank
+        let Webpage = document.createElement('p'); // fill in the blank
+        let Adress = document.createElement('p'); // fill in the blank
+        let Blurb = document.createElement('p'); // fill in the blank
+        let logo = document.createElement('img');
+        let url = document.createElement('a');
+        // let url = document.createElement('a');
+
+        // Build the h2 content out to show the Member's full name
+        Event.textContent = `${Member.Event} `; // fill in the blank
+        Webpage.textContent = `Webpage: ${Member.Webpage}`; // fill in the blank
+        Adress.textContent = `Adress: ${Member.Adress}`; // fill in the blank
+        Blurb.textContent = `${Member.Blurb}`; // fill in the blank
+        // Build the image logo by setting all the relevant attributes
+        logo.setAttribute('src', Member.logo);
+        logo.setAttribute('alt', `logo of ${Member.Event} `); // fill in the blank
+        logo.setAttribute('alt', `logo of ${Member.Event} `); // fill in the blank
+        url.setAttribute('href', `https://${Member.Webpage}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '340');
+        // logo.setAttribute('height', '440');
+        url.innerHTML = Member.Webpage;
+        // Append the section(card) with the created elements
+        card.appendChild(logo);
+        card.appendChild(Event);
+        card.appendChild(url); //fill in the blank
+        // card.appendChild(Webpage); //fill in the blank
+        card.appendChild(Adress); //fill in the blank
+        card.appendChild(Blurb); //fill in the blank
+
+        cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+}
+
+// const displaymembers = (members) => {
+//     // card build code goes here
+//     members.forEach((Member) => {
+
+//     });
+// }
+// async function getMemberData() {
+//     console.log('calling');
+//     const result = await resolveAfter2Seconds();
+//     console.log(result);
+//     // Expected output: "resolved"
+//   }
+//   getMemberData();
+
+async function getMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.discover); // temporary testing of data retreival
+    displaymembers(data.discover);
+}
+
+
+
+getMemberData();
