070a14def92ec08cd972ace5bbdea36cb1adb251
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-17 17:41:41 -0800

    frog

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -3,12 +3,12 @@ const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("🕶️")) {
-        main.style.background = "#000";
+        main.style.background = "#002e0a9f";
         main.style.color = "#fff";
         modeButton.textContent = "🔆";
     } else {
         main.style.background = "#eee";
         main.style.color = "#000";
-        modeButton.textContent = "🕶️";
+        modeButton.textContent = "🐸";
     }
 });
