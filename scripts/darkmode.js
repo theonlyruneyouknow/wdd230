24641dc05e4a3684ac1c01aa2c8db636440e4118
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-17 17:48:35 -0800

    1

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -2,8 +2,8 @@ const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("🕶️")) {
-        main.style.background = "#002e0a9f";
+    if (modeButton.textContent.includes("🐸")) {
+        main.style.background = "#02240f";
         main.style.color = "#fff";
         modeButton.textContent = "🔆";
     } else {
