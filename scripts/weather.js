0b4759c25152a2344453822ad26618d954232438
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-20 12:05:48 -0800

    1

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#figcaption');
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?lat=44.08&lon=-123.03&appid=9e7bbf1ed19a6be3b7439a78e5e9e589&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.08&lon=-123.03&appid=9e7bbf1ed19a6be3b7439a78e5e9e589&units=imperial';
 
 async function apiFetch() {
     try {
