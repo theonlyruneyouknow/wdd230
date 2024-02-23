20bc5a7f6d250bed429d8dac393f3a7944722884
Rune Larsen <theonlyruneyouknow@gmail.com>
2024-02-23 09:28:52 -0800

    2 tog

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -18,3 +18,12 @@ modeButton.addEventListener("click", () => {
         document.getElementByClass('grid').hidden = false
     }
 });
+
+function myFunction() {
+    var x = document.getElementById("myTable");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
